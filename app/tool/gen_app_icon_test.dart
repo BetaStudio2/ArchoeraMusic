ea302d985e6917127d907ca8f4d5e9cc537e53ec
BetaStudio2 <BetaStudio2@users.noreply.github.com>
2026-09-11 23:28:51 +0800

    chore(ui): material_ui 迁移遗漏 tool/gen_app_icon_test.dart

--- app/tool/gen_app_icon_test.dart
+++ app/tool/gen_app_icon_test.dart
@@ -10,7 +10,7 @@
 import 'dart:io';
 import 'dart:ui' as ui;
 
-import 'package:flutter/material.dart';
+import 'package:material_ui/material_ui.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
